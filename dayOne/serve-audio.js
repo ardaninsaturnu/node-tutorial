bcf787930fe74dce8e3933ffac5e1610679793cb
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-05 06:55:26 +0300

    audio integrated as well. @main

---
+++ dayOne/serve-audio.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer( ( request, response ) => {
+  
+  fs.exists('audio.mp3', function( exists ) {
+    if( exists ) {
+      const rstream = fs.createReadStream('audio.mp3');
+      rstream.pipe( response );
+    } else {
+      response.end('It is a 404')
+    }
+    
+  })
+  
+}).listen(3000)
