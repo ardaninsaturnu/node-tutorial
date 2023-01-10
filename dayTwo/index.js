01127e4bd15ed4d8b9d59b6938103d899160e884
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-10 06:43:39 +0300

    fs file management comment integrated.

--- dayTwo/index.js
+++ dayTwo/index.js
@@ -1,1 +1,16 @@
-console.log( 'day 2 just started.' )
+console.log( 'day 2 just started.' );
+
+
+/*
+* fs file works for file management.
+*
+* we cant manage all file with
+* const fs require('fs')
+*
+* rename ( asycrhnous, sychanrs )
+* create ( asycrhnous, sychanrs )
+* delete ( asycrhnous, sychanrs )
+* update ( asycrhnous, sychanrs )
+* read ( asycrhnous, sychanrs )
+*
+* */
