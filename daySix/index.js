eab7cb5a40c844cf8ac36c017c7b0aa61250ea2e
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-14 07:58:07 +0300

    day 6 done day 7 done.

---
+++ daySix/index.js
@@ -0,0 +1,18 @@
+// npm is most largest package manager in the world
+
+
+// to update npm with largest version by running => npm install npm@latest -g
+// to check on the version you can run on terminal => npm -v
+
+
+/*
+>npm outdated -g --depth=0
+Package             Current   Wanted   Latest  Location
+@angular/cli          1.1.3    1.4.2    1.4.2	xyz
+babel                 6.5.2   6.23.0   6.23.0	xyz
+babel-cli            6.18.0   6.26.0   6.26.0	xyz
+nodemon              1.11.0   1.12.1   1.12.1	xyz
+sails                0.11.0  0.12.13  0.12.13	xyz
+webpack              1.13.3    3.6.0    3.6.0	xyz
+webpack-dev-server   1.16.2    2.8.2    2.8.2	xyz
+*/
