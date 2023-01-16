7e82a9b3b8a2dcef5f6c0ce49ae3b491742a3f4f
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-16 14:38:07 +0300

    hashing a file.

--- dayNine/index.js
+++ dayNine/index.js
@@ -61,9 +61,40 @@ const generateHash = data.digest('hex');
 console.log(generateHash)
 */
 
+// encoding with SHA512 algorithm
+/*
 const crypto = require('crypto');
 const hash = crypto.createHash('SHA224');
 const data = hash.update('coelhoMamet', 'utf-8');
 const generateHash = data.digest('hex');
 console.log(generateHash)
+*/
+
+//loading the required modules in node.js
+const crypto = require('crypto');
+const fs = require('fs');
+
+//algoritm to be used for HASH
+const algorithm = 'sha256';
+const hash = crypto.createHash(algorithm);
+
+// reading the content of the file
+const filename = "./data.txt";
+const file_data = fs.ReadStream(filename);
+
+//passing the data to be hashed
+file_data.on('data', function( data ){
+  hash.update( data )
+})
+
+// Creating hash in the required format and writing it in file
+file_data.on('end', function () {
+  const gen_hash = hash.digest('hex')
+  console.log( 'Hash generated using' + algorithm  );
+  fs.writeFileSync( filename, gen_hash );
+  console.log(gen_hash)
+})
+
+
+
 
